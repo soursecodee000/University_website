23909b1b9494636d30e088e1c5e3e5553ddde3ac
Muhammad Ahmad <ma7744746@gmail.com>
2024-09-27 22:27:46 +0500

    first commit

---
+++ university_project/settings/dev.py
@@ -0,0 +1,18 @@
+from .common import *
+
+
+SECRET_KEY = 's3#tej=^qp^iwkfb889@2160&(&#&tr$ryzhx%!2n%!7m0!@d('
+
+DEBUG = True
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'school_project',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1122'
+    }
+}
