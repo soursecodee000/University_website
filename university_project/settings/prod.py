8db76d147312da81f01ea4976eb79551f06ce02d
Muhammad Ahmad <ma7744746@gmail.com>
2024-09-27 22:53:44 +0500

    starting deployment

--- university_project/settings/prod.py
+++ university_project/settings/prod.py
@@ -1,3 +1,4 @@
+import dj_database_url
 from .common import *
 import os
 
@@ -5,4 +6,16 @@ SECRET_KEY=os.environ['SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['uni-prod-31552cb0c12e.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+
+
+## email sending app
+EMAIL_HOST=os.environ['MAILGUN_SMTP_SERVER']
+EMAIL_HOST_USER=os.environ['MAILGUN_SMTP_LOGIN']
+EMAIL_HOST_PASSWORD=os.environ['MAILGUN_SMTP_PASSWORD']
+EMAIL_PORT=os.environ['MAILGUN_SMTP_PORT']
